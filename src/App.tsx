7c189631a860b698430201bd54d8827d09fa4464
YTRodi <yagorodi@gmail.com>
2022-03-01 01:02:27 -0300

    feat: Hero.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import {
-	Flex,
-	Title,
-	Subtitle,
-	Heading,
-	SubHeading,
-	Body,
-	ButtonText,
-	LinkText,
-} from '@/components';
-import { Navbar } from '@/components/Layout';
+import { Hero, Navbar } from '@/components/Layout';
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<Flex direction='column' align='center' justify='center'>
-				<Title>Title</Title>
-				<Subtitle>Subtitle</Subtitle>
-				<Heading>Heading</Heading>
-				<SubHeading>SubHeading</SubHeading>
-				<Body>Body</Body>
-				<ButtonText>ButtonText</ButtonText>
-				<LinkText>LinkText</LinkText>
-			</Flex>
+			<Hero />
 		</>
 	);
 }
--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,2 +1,3 @@
 export { default as Logo } from '@/components/Layout/Logo';
 export { default as Navbar } from '@/components/Layout/Navbar';
+export { default as Hero } from '@/components/Layout/Hero';

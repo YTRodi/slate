a5e4c56a187ea0ae2f093f94674017439be5869a
YTRodi <yagorodi@gmail.com>
2022-03-01 16:56:53 -0300

    feat: basic Button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Box } from '@/components/Box';
 export { default as Flex } from '@/components/Flex';
 export { default as Image } from '@/components/Image';
+export { default as Button } from '@/components/Button';
 
 // Text styles
 export { default as Text } from '@/components/Text';

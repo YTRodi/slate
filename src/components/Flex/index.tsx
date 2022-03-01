450616a73977e821ea8482bfa61695060cd23089
YTRodi <yagorodi@gmail.com>
2022-03-01 16:02:04 -0300

    update: remove unused gap prop in Flex component

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -28,35 +28,6 @@ export default styled('div', {
 			wrap: { fw: 'wrap' },
 			wrapReverse: { fw: 'wrap-reverse' },
 		},
-		// gap: {
-		// 	1: {
-		// 		gap: '$1',
-		// 	},
-		// 	2: {
-		// 		gap: '$2',
-		// 	},
-		// 	3: {
-		// 		gap: '$3',
-		// 	},
-		// 	4: {
-		// 		gap: '$4',
-		// 	},
-		// 	5: {
-		// 		gap: '$5',
-		// 	},
-		// 	6: {
-		// 		gap: '$6',
-		// 	},
-		// 	7: {
-		// 		gap: '$7',
-		// 	},
-		// 	8: {
-		// 		gap: '$8',
-		// 	},
-		// 	9: {
-		// 		gap: '$9',
-		// 	},
-		// },
 	},
 
 	defaultVariants: {

2f2c68bdfd9e349bdf4bfa74f5bd100d76613458
YTRodi <yagorodi@gmail.com>
2022-03-01 17:05:59 -0300

    feat: Hero (mobile first)

---
+++ src/components/Layout/Hero/Image/index.tsx
@@ -0,0 +1,10 @@
+import { Flex, Image } from '@/components';
+
+const HeroImage = () => (
+	<Flex direction='column'>
+		<Image src='/assets/images/hero/navigation.png' alt='navigation-hero' />
+		<Image src='/assets/images/hero/screen.png' alt='screen-hero' />
+	</Flex>
+);
+
+export default HeroImage;
--- src/components/Layout/Hero/index.tsx
+++ src/components/Layout/Hero/index.tsx
@@ -1,5 +1,17 @@
+import { Box, Flex, Button } from '@/components';
+import Title from './Title';
+import HeroImage from './Image';
+
 const Hero = () => {
-	return <h1>Hero</h1>;
+	return (
+		<Box css={{ py: 30, px: 20 }}>
+			<Flex direction='column' css={{ py: 50, gap: 60 }}>
+				<Title />
+				<HeroImage />
+				<Button appearance='primary'>Try for free</Button>
+			</Flex>
+		</Box>
+	);
 };
 
 export default Hero;
